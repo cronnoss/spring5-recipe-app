69769dae0691552d65b4c641eaaa494884560fe3
Evgeniy Karasev <karasevevg@gmail.com>
2021-09-19 16:02:25 +0300

    15. Assignment Review Display List of Recipes

--- src/main/java/com/cronnoss/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/cronnoss/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.cronnoss.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,16 +17,18 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
-    @Enumerated(value = EnumType.STRING)
-    private Difficulty difficulty;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -34,14 +37,14 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getDescription() {

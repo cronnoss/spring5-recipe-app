3751db013a29202364f4624214e48e7e18fecc37
Evgeniy Karasev <karasevevg@gmail.com>
2021-10-13 09:29:41 +0300

    10. Spring Integration Test

---
+++ src/test/java/com/cronnoss/spring5recipeapp/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,35 @@
+package com.cronnoss.spring5recipeapp.repositories;
+
+import com.cronnoss.spring5recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals(uomOptional.get().getDescription(), "Teaspoon");
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals(uomOptional.get().getDescription(), "Cup");
+    }
+}

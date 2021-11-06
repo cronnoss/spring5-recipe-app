201b93ef3f9d5cd76a53dac48d88457c51fb31ac
Evgeniy Karasev <karasevevg@gmail.com>
2021-11-06 12:42:35 +0300

    4. Spring MVC Exception Handler

--- src/main/java/com/cronnoss/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/cronnoss/spring5recipeapp/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.cronnoss.spring5recipeapp.controllers;
 
 import com.cronnoss.spring5recipeapp.commands.RecipeCommand;
+import com.cronnoss.spring5recipeapp.exceptions.NotFoundException;
 import com.cronnoss.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -51,5 +51,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
